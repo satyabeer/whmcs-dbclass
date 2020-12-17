60960e62b0d76b52f5e9435a755cb34b5cfb16d0
ShineD-153(Satya) <ShineD-153@domain.wgs.local>
2020-12-17 18:19:15 +0530

    Add and rename few functions

--- modules/addons/sampleaddon/lib/DBClass.php
+++ modules/addons/sampleaddon/lib/DBClass.php
@@ -1,5 +1,22 @@
 <?php
-/*
+
+/**
+ * Short description for file
+ *
+ * PHP version 7.4
+ *
+ * LICENSE: This source file is free to use, modify or share.
+ * The source code is available through the world-wide-web at the following URI:
+ * https://github.com/satyabeer/whmcs-dbclass.git.
+ *
+ * @category   WHMCS
+ * @package    Modules
+ * @author     Original Author <satyabeeryadav28@gmail.com>
+ * @version    Release: 1.0
+ * @link       https://github.com/satyabeer/whmcs-dbclass.git
+ */
+
+/**
  * Change the namespace with your moduletype and modulename
  * Example: namespace WHMCS\Module\Addons\SampleAddon; to namespace WHMCS\Module\{ModuleType}\{ModuleName};
 */
@@ -8,13 +25,29 @@ namespace WHMCS\Module\Addons\SampleAddon;
 
 use WHMCS\Database\Capsule;
 
-final class DBClass {
+final class DBClass
+{
+    /**
+     * Check the table existence
+     *
+     * @return boolean
+     */
+    public static function hasTable($table)
+    {
+        return Capsule::schema()->hasTable($table);
+    }
 
-    public static function createSchema($schemas) {
+    /**
+     * Create the tables
+     *
+     * @return void
+     */
+    public static function createTables($tables)
+    {
         try {
-            foreach($schemas as $table => $structure) {
+            foreach($tables as $table => $structure) {
                 // check table existence
-                if( !Capsule::schema()->hasTable($table) ) {
+                if( !self::hasTable($table) ) {
                     // create table structure
                     Capsule::schema()->create($table, function ($t) use($structure) {
                         foreach($structure as $column => $type) {
@@ -28,57 +61,119 @@ final class DBClass {
         }
     }
 
-    public static function getRow($table, $where) {
-        $query = Capsule::table($table);
-        $clause = '';
-        // handle between clause
-        if(array_key_exists('whereBetween', $where)) {
-            $clause .= self::recursiveClause($where['between']);
-        }
-        // handle notbetween clause
-        if(array_key_exists('whereNotBetween', $where)) {
-            $clause .= self::recursiveClause($where['whereNotBetween']);
-        }
-
-        $query = $query->$clause;
-
-        echo $query;die;
-
-        return Capsule::table($table)->where($where)->first();
-    }
-
-    public static function getResult($table, $where = null) {
-        if(is_null($where)) {
-            return Capsule::table($table)->get();
-        } else {
-            return Capsule::table($table)->where($where)->get();
-        }
-    }
-
-    public static function insertData($table, $data) {
+    /**
+     * Drop the tables
+     *
+     * @return void
+     */
+    public static function dropTable($tables)
+    {
         try {
-            $insertId = Capsule::table($table)->insert($data);
+            if (is_array($tables)) {
+                foreach($tables as $table) {
+                    // drop table
+                    Capsule::schema()->dropIfExists($table);
+                }
+            } else {
+                // drop table
+                Capsule::schema()->dropIfExists($tables);
+            }
+        } catch (\Throwable $e) {
+            logActivity("Unable to create Table: {$e->getMessage()}");
+        }
+    }
+
+    /**
+     * Insert row in table
+     *
+     * @return boolean
+     */
+    public static function insertRecord($table, $data)
+    {
+        try {
+           return Capsule::table($table)->insert($data);
         } catch (Exception $ex) {
             logActivity("Can not insert data into {$table}, error: {$ex->getMessage()}");
         }
-
-        return $insertId;
     }
 
-    public static function updateData($table, $data, $where = null) {
-        if(is_null($where)) {
-            return Capsule::table($table)->update($data);
-        } else {
-            return Capsule::table($table)->where($where)->update($data);
+    /**
+     * Insert row in table and return inserted id value
+     *
+     * @return int
+     */
+    public static function insertGetId($table, $data)
+    {
+        try {
+           return Capsule::table($table)->insertGetId($data);
+        } catch (Exception $ex) {
+            logActivity("Can not insert data into {$table}, error: {$ex->getMessage()}");
         }
     }
 
-    private function recursiveClause($clauseName) {
-        $clause = '';
-        foreach($clauseName as $column => $values) {
-            $clause .= array_keys($clauseName)($column, $values);
+    /**
+     * Update the table records
+     * return value will be the number of rows updated
+     *
+     * @return int
+     */
+    public static function updateRecord($table, $data, $where = null)
+    {
+        try {
+            if (is_null($where))
+                return Capsule::table($table)->update($data);
+            else
+                return Capsule::table($table)->where($where)->update($data);
+        } catch (Exception $ex) {
+            logActivity("Can not update record into {$table}, error: {$ex->getMessage()}");
         }
+    }
 
-        return $clause;
+    /**
+     * delete the rows from table
+     *
+     * @return int
+     */
+    public function deleteRecord($table, $where = null)
+    {
+        try {
+            if (is_null($where))
+                return Capsule::table($table)->delete();
+            else
+                return Capsule::table($table)->where($where)->delete();
+        } catch (Exception $ex) {
+            logActivity("Can not delete record from {$table}, error: {$ex->getMessage()}");
+        }
+    }
+
+    /**
+     * Get the single row of the table
+     *
+     * @return object
+     */
+    public static function getRow($table, $where)
+    {
+        try {
+            return Capsule::table($table)->where($where)->first();
+        } catch (Exception $ex) {
+            logActivity("Can not get data from {$table}, error: {$ex->getMessage()}");
+        }
+    }
+
+    /**
+     * Get the multiple rows of the table
+     *
+     * @return object
+     */
+    public static function getResult($table, $where = null)
+    {
+        try {
+            if (is_null($where))
+                return Capsule::table($table)->get();
+            else
+                return Capsule::table($table)->where($where)->get();
+        } catch (Exception $ex) {
+            logActivity("Can not get data from {$table}, error: {$ex->getMessage()}");
+        }
     }
 }
